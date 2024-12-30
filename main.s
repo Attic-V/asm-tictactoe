d09108a06110a0ab66787babf3c358f30922cbd1
Attic V <106038832+Attic-V@users.noreply.github.com>
2024-12-30 17:56:24 -0600

    create main

--- main.s
+++ main.s
@@ -2,6 +2,16 @@ section .text
 	global _start
 
 _start:
+	call    main
+
 	mov     rax, 60
 	mov     rdi, 0
 	syscall
+
+main:
+	push    rbp
+	mov     rbp, rsp
+
+	mov     rsp, rbp
+	pop     rbp
+	ret
