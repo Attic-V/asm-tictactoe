8b2361fbfa5e78195b36aa780dcf2c56015cd890
Attic V <106038832+Attic-V@users.noreply.github.com>
2026-05-21 18:07:01 -0500

    create nix dev shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+	description = "tic tac toe";
+
+	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+	outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in
+	{
+		devShells.${system}.default = pkgs.mkShell {
+			packages = [
+				pkgs.gnumake
+				pkgs.nasm
+			];
+		};
+	};
+}
